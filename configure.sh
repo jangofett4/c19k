bc920e28aafab69fec54ccce76f5d474a90b8124
Yahya Gedik <yahyagedikyg@gmail.com>
2023-05-18 20:37:53 +0300

    Fixed broken 'configure' script

--- configure.sh
+++ configure.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 mkdir -p obj bin iso
+git submodule init
 git submodule update
+cd limine
+make
+cd ..
 cp ./limine/limine-cd-efi.bin iso
 cp ./limine/limine-cd.bin iso
 cp ./limine/limine.sys iso
